10e9f3767b0aab0346827f272ea03a30f85fcb91
Rami-Alshembari <143111412+Rami-Alshembari@users.noreply.github.com>
2023-08-25 11:04:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f5f5f5;
-    margin: 0;
-    padding: 0;
-}
-
-header {
-    background-color: #2c3e50;
-    color: white;
-    padding: 20px 0;
-    border-bottom: 2px solid #34495e;
-}
-
-header h1 {
-    margin: 20px 0;
-    font-size: 2em;
-}
-
-header h2 {
-    margin: 10px 0;
-    font-size: 1.5em;
-}
-
-.section {
-    background-color: white;
-    padding: 20px;
-    border-radius: 5px;
-    border-left: 5px solid #2c3e50;
-}
-
-.section h3 {
-    border-bottom: 2px solid #2c3e50;
-    padding-bottom: 10px;
-}
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const target = document.getElementById(this.dataset.target);
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
