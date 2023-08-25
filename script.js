58fc8e0e5ee7ecb442e5de679f043ee5029e7893
Rami-Alshembari <143111412+Rami-Alshembari@users.noreply.github.com>
2023-08-25 10:58:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        
-        const target = document.getElementById(this.dataset.target);
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f5f5f5;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #2c3e50;
+    color: white;
+    padding: 20px 0;
+    border-bottom: 2px solid #34495e;
+}
+
+header h1 {
+    margin: 20px 0;
+    font-size: 2em;
+}
+
+header h2 {
+    margin: 10px 0;
+    font-size: 1.5em;
+}
+
+.contact-details p {
+    margin: 10px 0;
+}
+
+.section {
+    background-color: white;
+    margin-top: 20px;
+    padding: 20px;
+    border-radius: 5px;
+    border-left: 5px solid #2c3e50;
+}
+
+.section h3 {
+    border-bottom: 2px solid #2c3e50;
+    padding-bottom: 10px;
+}
+
+footer {
+    background-color: #2c3e50;
+    color: white;
+    text-align: center;
+    padding: 20px 0;
+    margin-top: 20px;
+}
